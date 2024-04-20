5b142545a6d6ded296bf80b060e044e320d2ac73
Alexander Thalhammer <alexander.thalhammer@gmail.com>
2024-04-20 17:20:27 +0200

    03 NgOptimizedImage with 3rd party image provider

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,6 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideHttpClient } from '@angular/common/http';
+import { provideImageKitLoader } from '@angular/common';
 import { provideRouter } from '@angular/router';
 
 import { appRoutes } from './app.routes';
@@ -8,6 +9,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideHttpClient(),
     provideZoneChangeDetection({ eventCoalescing: true }),
+    provideImageKitLoader('https://ik.imagekit.io/LXT'),
     provideRouter(
       appRoutes,
       // withDebugTracing(),
